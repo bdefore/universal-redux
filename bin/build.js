26485c07e6c92d4159d2170e5d574c561bd8e3ad
Buck DeFore <bdefore@gmail.com>
2015-11-27 12:29:31 -0800

    add better output for build

--- bin/build.js
+++ bin/build.js
@@ -15,6 +15,34 @@ webpackConfig.plugins.push(new webpack.DefinePlugin({
 }));
 
 console.log('Webpack config:', webpackConfig);
+console.log('\nBuilding...');
 webpack(webpackConfig, function(err, stats) {
-  console.log('Webpack build completed. Error?', err);
+  if(err) {
+    console.log('Webpack build had fatal error:', err);
+    return;
+  }
+
+  var options = {
+    hash: true,
+    version: true,
+    timings: true,
+    assets: true,
+    chunks: false,
+    colors: true
+  }
+
+  console.log('Webpack compile was successful.');
+
+  var jsonStats = stats.toJson();
+  if(jsonStats.errors.length > 0) {
+    console.log('Webpack had errors.');
+    options.errors = true;
+  }
+  if(jsonStats.warnings.length > 0) {
+    console.log('Webpack had warnings.');
+    options.warnings = true;
+  }
+
+  console.log(stats.toString(options));
+
 });
