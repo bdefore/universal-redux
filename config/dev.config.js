3df84558cce7417e468e39e39582327b3185fe85
Buck DeFore <bdefore@gmail.com>
2015-11-29 05:51:04 -0800

    disable devtools by default, can set in config

--- config/dev.config.js
+++ config/dev.config.js
@@ -97,8 +97,7 @@ module.exports = {
     new webpack.DefinePlugin({
       __CLIENT__: true,
       __SERVER__: false,
-      __DEVELOPMENT__: true,
-      __DEVTOOLS__: true  // <-------- DISABLE redux-devtools HERE
+      __DEVELOPMENT__: true
     }),
     webpackIsomorphicToolsPlugin.development()
   ]
