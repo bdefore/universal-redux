cbc58b4557567a69de205758165eb49b6cf92ae1
Buck DeFore <bdefore@gmail.com>
2015-12-01 14:46:29 -0800

    set redux logger default to false

--- bin/merge-configs.js
+++ bin/merge-configs.js
@@ -32,6 +32,7 @@ combinedWebpackConfig.plugins.push(isProduction ? toolsPlugin : toolsPlugin.deve
 
 // add default settings that are used by server via process.env
 var definitions = {
+  __LOGGER__: false,
   __DEVTOOLS__: !isProduction,
   __DEVELOPMENT__: !isProduction,
   'process.env': {
