1d59751fa5d2e584a56eb7509b2e0ce7e5e3cd5d
chrisblossom <cbloss@gmail.com>
2015-12-14 13:29:01 -0800

    formatting order

--- bin/npm-postinstall.js
+++ bin/npm-postinstall.js
@@ -1,19 +1,29 @@
 'use strict';
 /* eslint no-var:0, func-names:0 */
-
 var path = require('path');
-
-// See npm-cli.js L#26
-var npmPath = path.resolve(path.dirname(process.env.npm_execpath), '../lib');
-var npm = require(npmPath + '/npm.js');
-
 var devDependencies = require('../package.json').devDependencies;
 var install = [];
 var npmPackage;
+var npm;
 
+// return if not installed as a module
+if (process.env.PWD.split('/').indexOf('node_modules') === -1) {
+  return;
+}
+
+// Return if not installed via npm
+if (process.env.npm_execpath === undefined) {
+  return;
+}
+
+// return if production mode
 if ((process.env.NODE_ENV === 'production') || (process.env.npm_config_production === 'true')) {
   return;
 }
+
+// See https://github.com/npm/npm/blob/master/bin/npm-cli.js L#26
+npm = require(path.resolve(path.dirname(process.env.npm_execpath), '../lib') + '/npm.js');
+
 
 for (npmPackage in devDependencies) {
   if (devDependencies.hasOwnProperty(npmPackage)) {
@@ -25,15 +35,13 @@ if (install[0] === undefined) {
   return;
 }
 
-console.log(process.env);
-
-npm.load({}, function(error) {
+npm.load({}, function (error) {
   if (error) {
     console.log('NPM error', error);
     throw new Error(error);
   }
 
-  npm.commands.install(install, function(installError) {
+  npm.commands.install(install, function (installError) {
     if (installError) {
       console.log('NPM install error', installError);
       throw new Error(installError);
