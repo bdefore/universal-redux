dfda7f6d19438b7b7e5ef5d6c4dec12be1e1c799
Buck DeFore <bdefore@gmail.com>
2016-01-05 00:06:30 -0800

    no let

--- bin/user-config.js
+++ bin/user-config.js
@@ -1,11 +1,13 @@
 const path = require('path');
-let config;
 
-try {
-  config = require(path.resolve('config/universal-redux.config.js'));
-} catch (err) {
-  console.error('No configuration file provided, using defaults.');
-  return {};
+function getConfig() {
+  try {
+    const config = require(path.resolve('config/universal-redux.config.js'));
+    return config;
+  } catch (err) {
+    console.error('No configuration file provided, using defaults.');
+    return {};
+  }
 }
 
-module.exports = config;
+module.exports = getConfig();
