2dddf207d4e59025154fc076240eee09d6953e0b
Buck DeFore <bdefore@gmail.com>
2016-01-09 21:24:18 -0800

    Release 3.0.0-rc3

--- bin/user-config.js
+++ bin/user-config.js
@@ -1,11 +1,16 @@
 const path = require('path');
 
+const userConfigPath = path.join(process.cwd(), './config/universal-redux.config.js');
+console.log('getting config', userConfigPath);
+
 function getConfig() {
   try {
-    const config = require(path.resolve('config/universal-redux.config.js'));
+    console.log('Trying to load project config from ', userConfigPath);
+    const config = require(path.resolve(userConfigPath));
+    console.log('Loaded project level config', config);
     return config;
   } catch (err) {
-    console.error('No configuration file provided, using defaults.');
+    console.error('No configuration file provided, using defaults.', err);
     return {};
   }
 }
