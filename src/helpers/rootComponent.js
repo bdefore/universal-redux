232317d9751d77eabf56b3062e483181e26a1098
Buck DeFore <bdefore@gmail.com>
2016-01-22 08:09:47 -0800

    bring redux-async-connect in to default root component

--- src/helpers/rootComponent.js
+++ src/helpers/rootComponent.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { RouterContext, Router } from 'react-router';
+import { Router } from 'react-router';
+import { ReduxAsyncConnect, loadOnServer } from 'redux-async-connect';
 
 export function createForServer(store, renderProps) {
-  const root = (
-    <Provider store={store} key="provider">
-      <div>
-        <RouterContext {...renderProps} />
-      </div>
-    </Provider>
-  );
-  return Promise.resolve({root});
+  return new Promise((resolve, reject) => {
+    loadOnServer(renderProps, store)
+      .then(() => {
+        const root = (
+          <Provider store={store} key="provider">
+            <div>
+              <ReduxAsyncConnect {...renderProps} />
+            </div>
+          </Provider>
+        );
+        resolve({ root });
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  });
 }
 
 export function createForClient(store, {routes, history, devComponent}) {
