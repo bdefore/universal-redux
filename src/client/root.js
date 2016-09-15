7e69e5d8f4ef7b4efa415f714b9b1fb3fa6a5f84
Buck DeFore <bdefore@gmail.com>
2016-09-15 15:13:18 -0400

    react-router-scroll provider, bumped react-router dependency

--- src/client/root.js
+++ src/client/root.js
@@ -1,9 +1,13 @@
 import { includes } from 'lodash';
 import reduxAsyncConnectClient from './providers/redux-async-connect';
+import reactRouterScroll from './providers/react-router-scroll';
 import asyncPropsClient from './providers/async-props';
 
 export default function(store, providers, devComponent) {
   let client = reduxAsyncConnectClient;
+  if (includes(providers, 'react-router-scroll')) {
+    client = reactRouterScroll;
+  }
   if (includes(providers, 'async-props')) {
     client = asyncPropsClient;
   }
